2874417602917fd98cf59ee1757254dd7e0ce5e3
brunomps98 <brunomps98@hotmail.com>
2025-09-05 12:51:19 -0300

    Home y login funcional falta vehicle

---
+++ src/react/pages/vehicle/vehicle.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import './vehicle.css';
+
+
+const Vehicle = () => { 
+
+    return (
+        <h1>Flota de Vehiculos</h1>
+    );
+
+};
+
+export default Vehicle;
