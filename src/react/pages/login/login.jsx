adfadbdde59ffd0d529049168b01a10665efd58d
brunomps98 <brunomps98@hotmail.com>
2025-09-26 10:19:08 -0300

    Implementacion en android pero sin llamda a API

--- src/react/pages/login/login.jsx
+++ src/react/pages/login/login.jsx
@@ -14,7 +14,7 @@ const Login = () => {
         setError(null);
 
         try {
-            const response = await fetch('http://localhost:8080/api/login', {
+            const response = await fetch('/api/login', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
