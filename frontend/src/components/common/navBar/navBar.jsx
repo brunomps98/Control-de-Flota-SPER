d5d2d4ef8d747c263d43de659fc9387ec2970508
brunomps98 <brunomps98@hotmail.com>
2025-10-31 12:35:36 -0300

    Arreglo Navbar

--- frontend/src/components/common/navBar/navBar.jsx
+++ frontend/src/components/common/navBar/navBar.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import logo from '../../../assets/images/logo.png';
-import './navbar.css';
+import './navBar.css';
 
 const Navbar = ({ user }) => {
   const navigate = useNavigate();
